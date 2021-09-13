daefdc1db652ee4fd0c429909457d5e88fb42100
linyouxun <linyouxun@xiao100.com>
2021-09-13 09:10:18 +0800

    feat: const

---
+++ utils/constant.js
@@ -0,0 +1,36 @@
+// 花费类型
+export const typeList = [
+  {
+    name: "购物",
+    children: [
+      { name: "公司代购" },
+      { name: "淘宝" },
+      { name: "京东" },
+      { name: "拼多多" },
+      { name: "电影" },
+    ],
+  },
+  {
+    name: "伙食",
+    children: [
+      { name: "早餐" },
+      { name: "午饭" },
+      { name: "晚饭" },
+      { name: "夜宵" },
+      { name: "聚餐" },
+      { name: "饿了么" },
+      { name: "美团" },
+    ],
+  },
+  {
+    name: "交通",
+    children: [
+      { name: "公交" },
+      { name: "地铁" },
+      { name: "飞机" },
+      { name: "出租车" },
+      { name: "单车" },
+      { name: "摩的" },
+    ],
+  },
+];
